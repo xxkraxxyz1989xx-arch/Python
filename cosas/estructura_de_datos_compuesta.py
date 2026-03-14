3e01dd1410f7d675695cefdf2d4092db00e84964
XxkraxxyzxX <xxkraxxyz1989xx@gmail.com>
2026-03-14 17:52:25 -0500

    14/03/2026

--- cosas/estructura_de_datos_compuesta.py
+++ cosas/estructura_de_datos_compuesta.py
@@ -29,4 +29,4 @@ conjunto={1,2,3,"signo",True,12.5}
 tupla=(1,2,3,"signo",True,12.5)
 
 #en una tupla, cada elemento tiene un indice, que empieza desde 0, y puedes acceder a cada elemento usando su indice, por ejemplo, para acceder al primer elemento de la tupla, usarias tupla[0], para acceder al segundo elemento, usarias tupla[1], y asi sucesivamente
-#pero a diferencia de las listas, las tuplas son inmutables, es decir, no se pueden modificar despues de ser creadas, por lo que no puedes agregar, eliminar o cambiar elementos en una tupla, a diferencia de las listas, donde si puedes modificar los elementos despues de ser creadas12
+#pero a diferencia de las listas, las tuplas son inmutables, es decir, no se pueden modificar despues de ser creadas, por lo que no puedes agregar, eliminar o cambiar elementos en una tupla, a diferencia de las listas, donde si puedes modificar los elementos despues de ser creadas
